b59cf77634ff63d4b2cd8ecddca5c7073aa90f6f
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2024-09-09 01:07:28 +0500

    Add Discord entry command registerer

--- card_clash/settings.py
+++ card_clash/settings.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
