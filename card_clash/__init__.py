914feed640176518f83f3a6ddc1cbac96d1ded93
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2024-09-09 12:18:40 +0500

    Little refactoring

--- card_clash/__init__.py
+++ card_clash/__init__.py
@@ -1,24 +1,3 @@
-from os import environ
-
-import requests
 from dotenv.main import load_dotenv
 
 load_dotenv()
-url = f"https://discord.com/api/v10/applications/{environ["DISCORD_APP_ID"]}/commands"
-headers = {
-    "Authorization": "Bot " + environ["DISCORD_APP_TOKEN"],
-    "Content-Type": "application/json"
-}
-data = {
-    "name": "launch",
-    "description": "Launch Card Clash",
-    "type": 4,
-    "handler": 2
-}
-
-response = requests.post(url, headers=headers, json=data)
-
-if response.status_code == 200:
-    print("Entry Command Registered")
-else:
-    print("Entry Command Not Registered")
