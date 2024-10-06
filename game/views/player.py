7dbe63d925fe18fc1f8c832a019db43f07430989
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2024-10-06 21:07:04 +0500

    Remove async

--- game/views/player.py
+++ game/views/player.py
@@ -8,16 +8,15 @@ from game.models import Player
 
 
 class PlayerData(View):
-    async def get(self, request):
+    def get(self, request):
         player_id = loads(request.body.decode('utf-8')).get('player_id')
-        player = await Player.objects.aget(pk=player_id)
+        player = Player.objects.get(pk=player_id)
         return JsonResponse(player.to_json(), safe=False)
 
 
 class PlayerCards(View):
-    async def get(self, request):
+    def get(self, request):
         player_id = loads(request.body.decode('utf-8')).get('player_id')
-        player = await Player.objects.aget(pk=player_id)
+        player = Player.objects.get(pk=player_id)
         player_cards = player.playercard_set.all()
-        player_cards_data = await sync_to_async(list)(player_cards.values("card_id", "quantity"))
-        return JsonResponse(player_cards_data, safe=False)
+        return JsonResponse(list(player_cards.values("card_id", "quantity")), safe=False)
